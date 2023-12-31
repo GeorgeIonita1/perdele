57aec60e9a0433a28b6dd4484190d500a5168773
George <g3orrg301@gmail.com>
2023-12-31 15:25:25 +0200

    Footer styling improvements

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,19 @@
 import { footerLinks } from "@/lib/utils";
+import Link from "next/link";
+import { Button } from "./ui/button";
 
 function iterateFooterLinks() {
     return footerLinks.map((link: FooterLink, idx: number): any => {
         return (
             <div key={idx} className="max-md:py-4">
-                <h4 className="font-extrabold py-3">{link.title}</h4>
+                <h4 className="font-extrabold py-3 text-xl">{link.title}</h4>
                 {link.lines.map((line: string, index: number): any => (
-                    <h5 key={index} className="py-2">{line}</h5>
+                    <Link
+                        key={index} className="py-0 block"
+                        href='/detail/2'
+                    >
+                        <Button variant='link' className="text-white p-0"><h5>{line}</h5></Button>
+                    </Link>
                 ))}
             </div>
         )
@@ -15,12 +22,12 @@ function iterateFooterLinks() {
 
 function Footer() {
     return (
-        <footer>
-            <div className="md:flex justify-between md:w-4/6 mx-auto py-6 px-8">
+        <footer className="bg-black text-white mt-16">
+            <div className="container md:flex justify-between py-16 px-8">
                 {iterateFooterLinks()}
             </div>
 
-            <div className="w-full h-px mx-auto bg-slate-950" />
+            <div className="w-full h-px mx-auto bg-white" />
 
             <div className="flex justify-between items-center p-6">
                 <small>Copyright 2023 Perdele</small>
--- lib/utils.ts
+++ lib/utils.ts
@@ -28,7 +28,6 @@ export const footerLinks: FooterLink[] = [
       'Livingroom',
       'Bedroom',
       'Bathroom',
-      'Silk'
     ]
   }
 ];
