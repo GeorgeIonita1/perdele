f2946fedbc199fd7fd14e84feb98aa190da494fa
George <g3orrg301@gmail.com>
2023-12-31 15:38:01 +0200

    Product page styling improvements

--- app/detail/[id]/page.tsx
+++ app/detail/[id]/page.tsx
@@ -3,7 +3,6 @@ import CardDetailHead from "@/components/CardDetailHead";
 function CardDetail() {
     return (
         <div className="container">
-            <div>I am the card detail page</div>
             <CardDetailHead />
         </div>
     );
--- components/CardDetailHead.tsx
+++ components/CardDetailHead.tsx
@@ -2,21 +2,20 @@ import Image from "next/image";
 
 function CardDetailHead() {
     return (
-        <div className="md:grid md:grid-cols-12 md:gap-4">
-            <div className="col-span-5">
+        <div className="md:grid md:grid-cols-12 md:gap-16">
+            <div className="col-span-5 aspect-square">
                 <Image alt="Imagine cu perdele" src='/perdele.jpg' height={768} width={768} />
             </div>
-            <div className="col-span-7 flex flex-col gap-4">
-                <h2 className="font-extrabold text-2xl">Product Title</h2>
+            <div className="col-span-7 flex flex-col gap-12 max-md:mt-6">
+                <h1 className="font-extrabold text-4xl">Product Title</h1>
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
                     Nostrum inventore libero maxime, iusto consequatur 
                     voluptatibus necessitatibus facere, quaerat, 
                     assumenda tenetur voluptatem deserunt numquam quisquam 
                     nihil quas! Consequatur corrupti maxime accusantium!
                 </p>
-                <span className="block">$150.00 <s>$210.00</s></span>
-                <div className="w-full h-px bg-slate-950" />
-                <div className="flex gap-6">
+                <span className="block font-bold text-xl">$150.00 <s>$210.00</s></span>
+                <div className="flex flex-col gap-2">
                     <small>SKU: 38495</small>
                     <small>Brand: Nutline</small>
                     <small>Category: Face</small>
