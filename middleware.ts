1ce7271f1b4b9ff084def39aaa101a25e8f7e04a
George <g3orrg301@gmail.com>
2024-01-14 22:34:40 +0200

    Added webhook endpoint to test auth sync with database

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes: ['/', '/product/:path*']
+    publicRoutes: ['/', '/product/:path*', '/api/webhooks(.*)'],
 });
 
 export const config = {
