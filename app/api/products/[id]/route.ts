4583dbf04f03304f23695d9eaa4005d5d6899c67
george <g3orrg301@gmail.com>
2024-01-21 16:13:12 +0200

    Small type fix

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -2,7 +2,7 @@ import { getSingleProduct } from "@/lib/utils";
 
 export async function GET(
     request: Request,
-    { params }: { params: { id: string } }
+    { params }: { params: { id: number } }
 ) {
     const id = params.id;
     const data = await getSingleProduct(id);
