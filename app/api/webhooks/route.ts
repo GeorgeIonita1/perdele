cc4815b903309d37f4d860bdb226b7b5698b2e72
george_wsl <g3orrg301@gmail.com>
2024-01-28 12:38:58 +0200

    Test clerk user CRUD to production

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -55,6 +55,7 @@ export async function POST(req: Request) {
   // Get the ID and type
   const { id } = evt.data;
   const eventType = evt.type;
+  console.log(eventType, id)
 
   switch (eventType) {
     case CREATE_USER: {
