46e75255cfb028d3f4690bff6bf8f1c9cb5104f2
george <g3orrg301@gmail.com>
2024-02-12 00:54:02 +0200

    Contact form improvements

--- components/ContactDisplay/ContactDisplay.tsx
+++ components/ContactDisplay/ContactDisplay.tsx
@@ -2,16 +2,14 @@ import Image from "next/image";
 import ContactForm from "./ContactForm";
 
 function ContactDisplay() {
-
-
     return (
-        <div className='grid grid-cols-2'>
-            <div>
-                <Image alt="map" src="/map.png" width={800} height={600} />
-            </div>
-            <div className="p-12">
+        <div className='md:grid md:grid-cols-2'>
+            <div className="p-12 md:order-1">
                 <h2>Get in touch</h2>
                 <ContactForm />
+            </div>
+            <div>
+                <Image alt="map" src="/map.png" width={800} height={600} />
             </div>
         </div>
     );
